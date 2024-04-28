27b22a02e50bac8cba74af89c5c25b652efae293
Ayoub Rebhi <74020228+AyoubRebhi@users.noreply.github.com>
2024-04-28 03:11:49 +0200

    Fixing route error

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 
 #[Route('/projet')]
